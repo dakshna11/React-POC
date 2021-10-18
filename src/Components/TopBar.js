7eb9e90a89f1a126b532f3bf728729f740ec0405
dakshna11 <54468002+dakshna11@users.noreply.github.com>
2021-10-18 19:55:38 +0530

    Update TopBar.js

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import phone from './img/phone-call.png';
+import clock from './img/clock.png'
+import email from './img/email.png'
 function TopBar() {
     return (
         <div className="topbar">
-            <div><span id="logo-1">Auto</span><span id="logo-2">Wash</span></div>
-
-            <div>
-                <div class="top-bar-icon">
-                    <i class="far fa-clock"></i>
-                </div>
-                <h3>Opening Hour</h3>
-                <p>Mon - Fri, 8.00 - 9.00</p>
+            <div style={{marginTop:'1%',marginLeft:'-6%'}}>
+                <span id="logo-1">Auto</span><span id="logo-2">Wash</span>
             </div>
-             
-            <div><h3>Call Us</h3><p>+012 345 6789</p></div>
-            <div><h3>Email Us</h3><p>info@example.com</p></div>
+            <div className='topbar-main'>
+                <div className='topbar-content-main'>
+                    <img src={clock} height='35px' width='35px' style={{marginTop:'20%'}}></img>
+                    <div className='topbar-content'> 
+                        <h3 style={{marginBottom:'0px'}}>Opening Hour</h3>
+                        <p style={{width:'150px'}}>Mon - Fri, 8.00 - 9.00</p>
+                    </div>
+                </div>
+                <div className='topbar-content-main'>
+                    <img src={phone} height='35px' width='35px' style={{marginTop:'20%'}}></img>
+                    <div className='topbar-content'> 
+                        <h3 style={{marginBottom:'0px'}}>Call Us</h3>
+                        <p style={{width:'150px'}}>+012 345 6789</p>
+                    </div>
+                </div>
+                <div  className='topbar-content-main' style={{marginLeft:'2%'}}>
+                <img src={email} height='35px' width='35px' style={{marginTop:'20%'}}></img>
+                <div className='topbar-content'> 
+                    <h3 style={{marginBottom:'0px'}}>Email Us</h3>
+                    <p style={{width:'150px'}}>info@example.com</p>
+                    </div>
+                </div>
+            </div>
         </div>
         
     );
